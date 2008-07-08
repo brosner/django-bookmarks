a618a549e519a6231dc41e461fdf2ea7b7861988
James Tauber <jtauber@jtauber.com>
2008-07-08 22:49:37 +0000

    fixed redirect required bug
    git-svn-id: http://django-bookmarks.googlecode.com/svn/trunk@11 413268e4-d34f-0410-bd0d-61523dc7b0b6

--- bookmarks/forms.py
+++ bookmarks/forms.py
@@ -7,7 +7,7 @@ class BookmarkForm(forms.ModelForm):
     
     url = forms.URLField(label = "URL", verify_exists=True, widget=forms.TextInput(attrs={"size": 40}))
     description = forms.CharField(max_length=100, widget=forms.TextInput(attrs={"size": 40}))
-    redirect = forms.BooleanField(label="Redirect")
+    redirect = forms.BooleanField(label="Redirect", required=False)
     
     def clean_url(self):
         if Bookmark.objects.filter(url=self.cleaned_data["url"]).count() > 0:
